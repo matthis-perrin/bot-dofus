2888e53dbc2176944a3a68ed0298dd8b4aabe67d
Matthis Perrin <matthis.perrin@gmail.com>
2021-11-16 18:44:17 +0100

    Display soleil location and score on the map

--- bot-ui/src/screenshot_view.tsx
+++ bot-ui/src/screenshot_view.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {gameCoordinates} from '../../common/model';
+import {gameCoordinates, SQUARE_SIZE} from '../../common/model';
 import {useServerState} from './events';
+
+// const soleilCoordinates = {
+//   x: gameCoordinates.x + ((best.x + 0.5) * SQUARE_SIZE.width) / 2,
+//   y: gameCoordinates.y + ((best.y + 0.5) * SQUARE_SIZE.height) / 2,
+// };
 
 export const ScreenshotView: React.FC = () => {
   const serverState = useServerState();
+  const soleils = serverState.soleil.filter(s => s.label === 'OK');
+
   return (
-    <Img
-      style={{width: gameCoordinates.width, height: gameCoordinates.height}}
-      src={`data:image/png;base64,${serverState.screenshot}`}
-    />
+    <Wrapper>
+      <Img
+        style={{width: gameCoordinates.width, height: gameCoordinates.height}}
+        src={`data:image/png;base64,${serverState.screenshot}`}
+      />
+      {soleils.map(s => (
+        <SoleilWrapper
+          key={`${s.x},${s.y}`}
+          style={{
+            left: (s.x * SQUARE_SIZE.width) / 2,
+            top: (s.y * SQUARE_SIZE.height) / 2,
+          }}
+        >{`${Math.round(s.score * 100 * 100) / 100}%`}</SoleilWrapper>
+      ))}
+    </Wrapper>
   );
 };
 ScreenshotView.displayName = 'ScreenshotView';
 
+const Wrapper = styled.div`
+  position: relative;
+`;
 const Img = styled.img``;
+const SoleilWrapper = styled.div`
+  position: absolute;
+  border: solid 2px red;
+  width: ${SQUARE_SIZE.width / 2}px;
+  height: ${SQUARE_SIZE.height / 2}px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  font-size: 10px;
+  font-weight: 500;
+`;
