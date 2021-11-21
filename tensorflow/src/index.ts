35216a79252e1922734ea5e597e79a02bdf9a7cf
Matthis Perrin <matthis.perrin@gmail.com>
2021-11-21 16:19:58 +0100

    Add fish_popup model generator

--- tensorflow/src/index.ts
+++ tensorflow/src/index.ts
@@ -1,12 +1,15 @@
+import {runFishPopup} from './fish_popup';
 import {runMap} from './map';
 import {runSoleil} from './soleil';
 
-const mode = 'map' as string;
+const mode = 'fish_popup' as string;
 
 if (mode === 'map') {
   runMap().catch(console.error);
 } else if (mode === 'soleil') {
   runSoleil().catch(console.error);
+} else if (mode === 'fish_popup') {
+  runFishPopup().catch(console.error);
 } else {
   console.error(`Unknown mode "${mode}"`);
 }
