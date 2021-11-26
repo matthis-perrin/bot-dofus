4d753adac1deb7f3d3ecae621242b3412659ec2c
Matthis Perrin <matthis.perrin@gmail.com>
2021-11-26 22:36:21 +0100

    Increase tolerance

--- bot/src/colors.ts
+++ bot/src/colors.ts
@@ -50,5 +50,5 @@ export function checkForColor(coordinates: Coordinate[], targetColor: string): b
   const colorAverage = getColorAverage(coordinates);
   const distance = colorDistance(colorAverage, color);
   console.log(colorAverage, color, distance);
-  return distance <= 10;
+  return distance <= 15;
 }
