54d27b8d77b4578e4b3cac577a95b12927ccad1a
Matthis Perrin <matthis.perrin@gmail.com>
2021-12-21 14:57:48 +0100

    Put back frontend

--- bot-ui/src/app.tsx
+++ bot-ui/src/app.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {CharacterForm} from './character_form';
 import {Modules} from './modules';
 import {ScreenshotView} from './screenshot_view';
 
 export const App: React.FC = () => {
   return (
     <Wrapper>
-      <CharacterForm />
-      {/* <ScreenshotView />
-      <Modules /> */}
+      <ScreenshotView />
+      <Modules />
     </Wrapper>
   );
 };
